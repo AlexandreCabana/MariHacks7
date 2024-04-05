a30ed7331d86ab4085ab68366acec7fb4b779a30
poutinedude <priggen@educ.college-montreal.qc.ca>
2024-04-05 18:45:51 -0400

    du html qui marche bitc

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form, status, Depends
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
+
+templates = Jinja2Templates(directory="html")
 app = FastAPI()
+
+app.mount("/css", StaticFiles(directory="css"), name="css")
+#app.mount("/Image", StaticFiles(directory="Image"), name="Image")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/")
+async def solve_equation(request: Request, equation: str = Form(...)) -> RedirectResponse:
+    print(equation)
+    return RedirectResponse("http://127.0.0.1:8000", status_code=status.HTTP_303_SEE_OTHER)
