a3ed25addbbbf9eb4a9d0ae6555a7c2eb2eba8a2
poutinedude <priggen@educ.college-montreal.qc.ca>
2024-04-05 22:14:24 -0400

    je peux trouver inconnu le degree et le coeficien

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+import string
+from terme import Terme
 
+alphabets = list(string.ascii_letters)
 
 templates = Jinja2Templates(directory="html")
 app = FastAPI()
@@ -25,7 +28,30 @@ async def solve_equation(request: Request, equation: str = Form(...)) -> Redirec
 
 def parser(equation: str):
     nb_terme = 1
+    #equation_parser = [Terme(3,"x",1)]
+    terme = ""
+    equation = list(equation)
+
+    #trouver le nombre de Terme
     for i in range(len(equation)):
         if equation[i] == '+' or equation[i] == '-':
             nb_terme += 1
+    terme = equation[0:equation.index('+')]
+    inconnue = ''
+    degree = 1
+    for i in range(len(terme)):
+        for j in range(len(alphabets)):
+            if terme[i] == alphabets[j]:
+                inconnue = terme[i]
+        if terme[i] == '^':
+            degree = "".join(terme[terme.index('{')+1 : terme.index('}')])
+
+    if inconnue == '':
+        degree = 0
+    coefficient = "".join(terme[0:terme.index(inconnue)])
+    print(inconnue)
+    print(degree)
+    print(coefficient)
+    print(terme)
     print(nb_terme)
+    Terme(int(degree), inconnue, int(coefficient))
