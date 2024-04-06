19f2eb62bd416bdad7d368c740b6c7c6b4e7591b
AlexandreCabana <101986583+AlexandreCabana@users.noreply.github.com>
2024-04-06 14:28:16 +0000

    ok c toute push sa mere

--- operations.py
+++ operations.py
@@ -3,13 +3,19 @@
 class Somme:
     def __init__(self, exprs):
         self.exprs = exprs
+    def __repr__(self):
+        return "Somme" + str(self.exprs)
 
 class Mult:
     def __init__(self, exprs):
         self.exprs = exprs
+    def __repr__(self):
+        return "Mult" + str(self.exprs)
 
 class Div:
     def __init__(self, num, denom):
         self.num = num
         self.denom = denom
+    def __repr__(self):
+        return "Div[" + str(self.num) + "]/[" + str(self.denom) + "]"
 
